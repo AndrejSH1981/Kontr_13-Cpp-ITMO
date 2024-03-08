19eda134a5f7e04d2ef8846c30fd395ff77e28d2
Андрей <133521547+AndrejSH1981@users.noreply.github.com>
2024-03-08 12:32:29 +0300

    Update teacher.h

--- Kontr_13-Cpp-ITMO/teacher.h
+++ Kontr_13-Cpp-ITMO/teacher.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "human.h"
 #include <string>
+# include <windows.h>
 
 // teacher.h
 //  Класс teacher производный от класса human
@@ -9,22 +10,23 @@ using namespace std;
 class teacher : public human {
     // Конструктор класса teacher
 public:
-    teacher(
-        string last_name,
-        string name,
-        string second_name,
 
-        // Количество учебных часов за семестр у преподавателя
+    teacher(string last_name, 
+        string name, 
+        string second_name, 
         unsigned int work_time
     )
-        : human(last_name, name, second_name) {
-        this->work_time = work_time;
+        : human(last_name, name, second_name, get_work_time(work_time)) {
+       // this->work_time = work_time;
     }
 
     // Получение количества учебных часов
-    virtual unsigned int get_work_time() {
-        return this->work_time;
+    string get_work_time(unsigned int work_time) {
+        this->work_time = work_time;
+        string str = "\nколичество часов = " + to_string(this->work_time) + ";";
+        return str;
     }
+
 private:
     // Учебные часы
     unsigned int work_time;
