a3cb69cfa039926c8158a5b8c1a65b8ef9ef780f
Андрей <133521547+AndrejSH1981@users.noreply.github.com>
2024-03-08 12:32:51 +0300

    Update human.h

--- Kontr_13-Cpp-ITMO/human.h
+++ Kontr_13-Cpp-ITMO/human.h
@@ -2,24 +2,25 @@
 #include <string>
 #include <sstream>
 #pragma once /* Защита от двойного подключения заголовочного файла */
-// human.h.
+// файл human.h.
 
 using namespace std;
 
 class human {
 public:
     // Конструктор класса human
-    human(string last_name, string name, string second_name) {
+    human(string last_name, string name, string second_name, string a) {
         this->last_name = last_name;
         this->name = name;
         this->second_name = second_name;
+        this->a2 = a;
     }
 
     // Получение ФИО человека
     virtual string get_full_name() {
         ostringstream full_name;
         full_name << this->last_name << " " << this->name << " "
-            << this->second_name;
+            << this->second_name << ", " << this -> a2;
         return full_name.str();
     }
 
@@ -27,4 +28,6 @@ private:
     string name;  // имя
     string last_name;  // фамилия
     string second_name; // отчество
+    string a2; // переменная для возврата значения
+ 
 };
